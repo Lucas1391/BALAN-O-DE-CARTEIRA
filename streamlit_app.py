9ffa5e68e35d480e79c71aae1eaf44fb2e59c660
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-07-01 20:39:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,35 @@
-from collections import namedtuple
-import altair as alt
-import math
+from PIL import Image
 import pandas as pd
 import streamlit as st
+import yfinance as yf
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def Main():
+    IVVB11 = yf.download("IVVB11.SA",period="10y")
+    PRECO_IVVB11 = IVVB11["Close"].iloc[-1]
+    IVVB11_HIGH = IVVB11['High'].max()
+    GOLD = yf.download("GC=F",period="10y")
+    PRECO_GOLD = GOLD["Close"].iloc[-1]
+    GOLD_HIGH = GOLD['High'].max()
+    BOVESPA = yf.download("^BVSP",period ="10y")
+    BOVESPA_HIGH = BOVESPA['High'].max()
+    PRECO_BOVESPA = BOVESPA["Close"].iloc[-1]
+    BOVESPA_HIGH = BOVESPA['High'].max()
+    GOLD11 = yf.download("GOLD11.SA",period="1d")
+    BOVA11 = yf.download("BOVA11.SA",period="1d")
+    COLUNAS = ["Topo Historico","Cotação Atual","Relativo","Ajustado","Percentual",]
+    CAPITAL = 7500.00
+    DADOS = pd.DataFrame(columns=COLUNAS)
+    DADOS["Topo Historico"] = [BOVESPA_HIGH,IVVB11_HIGH,GOLD_HIGH]
+    DADOS.index = ["IBOV","IVVB11","GOLD"]
+    DADOS['Cotação Atual'] = [PRECO_BOVESPA,PRECO_IVVB11,PRECO_GOLD]
+    FATOR = 0.60*DADOS["Topo Historico"]
+    AMPLITUDE = DADOS["Topo Historico"] - DADOS['Cotação Atual']
+    DADOS['Relativo'] = AMPLITUDE/FATOR
+    DADOS.iloc[2,2] = 2*DADOS.iloc[2,2]
+    #DADOS['Relativo'] = AMPLITUDE
+    SOMA = DADOS['Relativo'].sum()
+    DADOS['Ajustado'] = 100*(DADOS['Relativo']/SOMA)
+    DADOS['Percentual'] = (25.00 + DADOS['Ajustado']/4.00)/(100.00)
+    DADOS['Valor por ativo'] = CAPITAL*DADOS['Percentual']
+    return DADOS
+  st.
